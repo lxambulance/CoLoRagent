92088124444b2a5e117aeed84d2eb5dce44ac21b
lxambulance <751795314@qq.com>
2021-03-17 20:13:02 +0800

    little change on main

--- SourceCode/main.py
+++ SourceCode/main.py
@@ -1,12 +1,17 @@
 # 主调度进程
 import ColorMonitor as CM
 
+from PyQt5.QtWidgets import QApplication
+
+from MainWindow import MainWindow
+
+import sys
+
 if __name__ == '__main__':
-    
-    CM.PL.Nid = 1
-    CM.PL.AddCacheSidUnit('F:\\研一下\\系统搭建\\代码实现\\test1.txt',1,0,0,0)
-    CM.PL.AddCacheSidUnit('F:\\研一下\\系统搭建\\代码实现\\kebiao.png',1,0,0,0)
-    CM.PL.SidAnn()
-    
     thread_monitor = CM.Monitor()
     thread_monitor.start()
+
+    app = QApplication([])
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
