d1d2bc8f979319dbde51a85df45489a77c832fae
Xiang Li <idealee@buaa.edu.cn>
2021-03-23 17:04:48 +0800

    update class AnnSid

--- SourceCode/main.py
+++ SourceCode/main.py
@@ -10,9 +10,13 @@ from PyQt5.QtWidgets import QApplication
 from MainWindow import MainWindow
 
 if __name__ == '__main__':
+    CM.PL.Nid = 1 # 初始化本机NID
+    
     thread_monitor = CM.Monitor()
     thread_monitor.setDaemon(True)
     thread_monitor.start()
+
+    CM.PL.AnnProxy()# 向RM注册本代理
 
     app = QApplication([])
     window = MainWindow()
