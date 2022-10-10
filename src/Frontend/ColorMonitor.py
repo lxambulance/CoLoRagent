b27358303c6de14f746b0e4ca6fea20644431a76
lxambulance <751795314@qq.com>
2022-10-10 11:25:49 +0800

    fix(pathdata): data包收包传前端参数错误

--- src/Frontend/ColorMonitor.py
+++ src/Frontend/ColorMonitor.py
@@ -433,7 +433,7 @@ class PktHandler(threading.Thread):
                                                                  '').zfill(40)
                     # 暂时将全部收到的校验和正确的data包显示出来
                     self.signals.pathdata.emit(
-                        0x73 | (RecvDataPkt.B << 8), NewSid, RecvDataPkt.PIDs, RecvDataPkt.PktLength, f'{NewGetPkt.nid:032x}')
+                        0x73 | (RecvDataPkt.B << 8), NewSid, RecvDataPkt.PIDs, RecvDataPkt.PktLength, f'{RecvDataPkt.nid_pro:032x}')
                     if RecvDataPkt.B == 0:
                         # 收到数据包
                         # 判断是否为当前代理请求内容
